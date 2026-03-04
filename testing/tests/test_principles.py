57ef059f7a2d66b175d0816b3b05a11768cd2860
Roman Satin <gans2010@list.ru>
2026-03-04 16:13:31 +0800

    Showed that tests dont guarantee that code is correct

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -1,8 +1,13 @@
 import sys
 sys.path.append('../src')
-from math_demo import add
+from math_demo import (add, add_with_bug)
 def test_addition():
     assert add(2, 2) == 4
     print('test basic addition')
+def test_addition_with_bug():
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    print('test basic addition with bugs')
 if __name__ == '__main__':
     test_addition()
+    test_addition_with_bug()

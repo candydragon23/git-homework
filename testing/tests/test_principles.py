aa672f3ee534592d9ad96654e75ccf62ebf049e2
Roman Satin <gans2010@list.ru>
2026-03-04 16:38:02 +0800

    Added reasonable amount of testing cases

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -19,8 +19,21 @@ def test_addition_overcomplicated():
             assert add(-i, -j) == sum(-i, -j)
             assert add(-i, j) == sum(-i, j)
             assert add(i, -j) == sum(i, -j)
+def test_addition_reasonable():
+    assert add(2, 2) == 4
+    assert add(0, 0) == 0
+    assert add(6, 7) == 13
+    assert add(-6, -7) == -13
+    assert add(7, 0) == 7
+    print('test addition reasonable')
+def test_addition_commutative():
+    assert add(-6, 7) == 1
+    assert add(7, -6) == 1
+    print('test addition commutative')
 if __name__ == '__main__':
     test_addition()
     test_addition_with_bug()
     test_addition_duplicated()
     #test_addition_overcomplicated()
+    test_addition_reasonable()
+    test_addition_commutative()

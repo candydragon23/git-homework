949bc1fd9c0d919abe2eb9a71355df6d1e7a67c8
candydragon23 <gans2010@list.ru>
2026-03-04 15:38:25 +0800

    Created pinger

--- pinger/pinger.py
+++ pinger/pinger.py
@@ -1,9 +1,9 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 
-now = datetime.now()
+current = datetime.now()
+url_template = "https://simurg.space/gen_file?data=obs&date={date}"
 while True:
-    url_template = "https://simurg.space/gen_file?data=obs&date={date}"
     date = now.strftime("%Y-%m-%d")
     url = url_template.format(date=date)
 
@@ -12,3 +12,5 @@ while True:
     if response.status_code == 200:
         print("Last available data are for {date}")
         break
+    else:
+        current = current - timedelta(days = 1)

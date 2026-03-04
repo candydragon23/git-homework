80f8e09d9534dd065cf405e444d4ad1710e79175
Roman Satin <gans2010@list.ru>
2026-03-04 16:20:30 +0800

    Showed to not duplicate logic of tested functions

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -8,6 +8,12 @@ def test_addition_with_bug():
     assert add_with_bug(2, 2) == 4
     assert add_with_bug(0, 0) == 0
     print('test basic addition with bugs')
+    #assert add_with_bug(6, 7) == 13 # will fail here
+def test_addition_duplicated():
+    #is it a real good test (relies on abscence of + in add)
+    assert add(2, 3) == 2 + 3
+    print('test addition duplicated')
 if __name__ == '__main__':
     test_addition()
     test_addition_with_bug()
+    test_addition_duplicated()
